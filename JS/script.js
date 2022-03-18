554dc6d7d620431de2919d3acd342d17c32224b1
brunoLombardi9 <95322998+brunoLombardi9@users.noreply.github.com>
2022-03-18 14:16:16 -0300

    Se agregan imagenes

--- JS/script.js
+++ JS/script.js
@@ -5,89 +5,79 @@
 const zapatillas = [];
 const contenedorProductos = document.querySelector('#contenedorProductos');
 
-// FUNCIONES ---------------------------------------------------------------------------
-
 class calzado {
-  constructor(marca, modelo, precio, categoria) {
+  constructor(marca, modelo, precio, categoria, imgOrigen) {
     this.marca = marca;
     this.modelo = modelo;
     this.precio = precio;
     this.categoria = categoria
     this.id = (zapatillas.length);
+    const imagen = new Image(imgOrigen);
+    imagen.src = imgOrigen;
+    this.img = imagen;
   }
 }
 
-// function cardsProductos() {
-//
-//   const card = Object.assign(document.createElement('div'), {
-//     className: 'col-lg-3 col-sm-6 bg-light card mt-3 mb-3 pb-3'
-//   });
-//
-//   card.innerHTML = `<img src="" class="img-fluid mt-2" alt="imagenProducto"><h2 class="text-center">${zapatilla.modelo}</h2><h3 class="text-center">$${zapatilla.precio}</h3>`
-//
-//   const contenedorProductos = document.querySelector("#contenedorProductos");
-//
-//   contenedorProductos.appendChild(card);
-// }
+
+// FUNCIONES ---------------------------------------------------------------------------
+
 
 function resultadoBusquedaMarcas() {
 
-  document.querySelectorAll('.botonesMarcas').forEach(botonMarca =>{
-  botonMarca.addEventListener('click', () => {
+  document.querySelectorAll('.botonesMarcas').forEach(botonMarca => {
+    botonMarca.addEventListener('click', () => {
 
-    const resultado = zapatillas.filter((zapatilla) => {
-      return zapatilla.marca === botonMarca.innerText;
+      const resultado = zapatillas.filter((zapatilla) => {
+        return zapatilla.marca === botonMarca.innerText;
+      })
+
+      contenedorProductos.innerHTML = '';
+
+      resultado.forEach((zapatilla) => {
+        const card = Object.assign(document.createElement('div'), {
+          className: 'col-lg-3 col-sm-6 bg-light card mt-3 mb-3 pb-3'
+        });
+
+        card.innerHTML = `<img src= ${zapatilla.img.src} class="img-fluid mt-2" alt="imagenProducto"><h2 class="text-center">${zapatilla.modelo}</h2><h3 class="text-center">$${zapatilla.precio}</h3>`
+
+        const contenedorProductos = document.querySelector("#contenedorProductos");
+
+        contenedorProductos.appendChild(card);
+      })
     })
-
-contenedorProductos.innerHTML = '';
-
-    resultado.forEach((zapatilla) => {
-      const card = Object.assign(document.createElement('div'), {
-        className: 'col-lg-3 col-sm-6 bg-light card mt-3 mb-3 pb-3'
-      });
-
-      card.innerHTML = `<img src="" class="img-fluid mt-2" alt="imagenProducto"><h2 class="text-center">${zapatilla.modelo}</h2><h3 class="text-center">$${zapatilla.precio}</h3>`
-
-      const contenedorProductos = document.querySelector("#contenedorProductos");
-
-      contenedorProductos.appendChild(card);
-    })
-
-  })
   })
 }
 
 function resultadoBusquedaCategorias() {
 
-  document.querySelectorAll('.botonesCategorias').forEach(botonCategoria =>{
-  botonCategoria.addEventListener('click', () => {
+  document.querySelectorAll('.botonesCategorias').forEach(botonCategoria => {
+    botonCategoria.addEventListener('click', () => {
 
-    const resultado = zapatillas.filter((zapatilla) => {
-      return zapatilla.categoria === botonCategoria.innerText;
+      const resultado = zapatillas.filter((zapatilla) => {
+        return zapatilla.categoria === botonCategoria.innerText;
+      })
+
+      contenedorProductos.innerHTML = '';
+
+      resultado.forEach((zapatilla) => {
+        const card = Object.assign(document.createElement('div'), {
+          className: 'col-lg-3 col-sm-6 bg-light card mt-3 mb-3 pb-3'
+        });
+
+        card.innerHTML = `<img src="" class="img-fluid mt-2" alt="imagenProducto"><h2 class="text-center">${zapatilla.modelo}</h2><h3 class="text-center">$${zapatilla.precio}</h3>`
+
+        const contenedorProductos = document.querySelector("#contenedorProductos");
+
+        contenedorProductos.appendChild(card);
+      })
     })
-
-contenedorProductos.innerHTML = '';
-
-    resultado.forEach((zapatilla) => {
-      const card = Object.assign(document.createElement('div'), {
-        className: 'col-lg-3 col-sm-6 bg-light card mt-3 mb-3 pb-3'
-      });
-
-      card.innerHTML = `<img src="" class="img-fluid mt-2" alt="imagenProducto"><h2 class="text-center">${zapatilla.modelo}</h2><h3 class="text-center">$${zapatilla.precio}</h3>`
-
-      const contenedorProductos = document.querySelector("#contenedorProductos");
-
-      contenedorProductos.appendChild(card);
-    })
-
-  })
   })
 }
 
 
 function init() {
   resultadoBusquedaMarcas();
-resultadoBusquedaCategorias();
+  resultadoBusquedaCategorias();
 }
 
 
@@ -95,13 +85,13 @@ resultadoBusquedaCategorias();
 
 
 
-const cortez = new calzado("Nike", "cortez", 15000, "Moda");
+const cortez = new calzado("Nike", "Cortez", 15000, "Moda", "imagenes/nike cortez.png");
 zapatillas.push(cortez);
 
-const alleyoop = new calzado("Nike", "alleyoop", 18000, "Skateboarding");
+const alleyoop = new calzado("Nike", "Alleyoop", 18000, "Skateboarding", "imagenes/nike aleyoop.jpg");
 zapatillas.push(alleyoop);
 
-const blazer = new calzado("Nike", "blazer", 14000, "Skateboarding");
+const blazer = new calzado("Nike", "Blazer", 14000, "Skateboarding", "imagenes/nike blazer.webp");
 zapatillas.push(blazer);
 
 const runFalcon = new calzado("Adidas", "run falcon", 12000, "Running");
